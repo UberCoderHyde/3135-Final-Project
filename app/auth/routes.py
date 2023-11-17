f37301bb486c1c06664b3a2f92bb3ff26864bc2c
Shlok Singh <ssingh67@uncc.edu>
2023-11-16 19:56:32 -0500

    testing routes.py updates

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,4 +2,7 @@ from flask import render_template, request, Blueprint
 
 from flask import Blueprint
 
-auth = Blueprint('auth', __name__, template_folder='templates')
+auth = Blueprint('auth', __name__, template_folder='templates', url_prefix = "/auth")
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
