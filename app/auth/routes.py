029d7eb318efc38ce147e40ad3db04bff359b91c
Shlok Singh <ssingh67@uncc.edu>
2023-11-16 20:22:32 -0500

    added all the routes for courses

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,3 +6,11 @@ auth = Blueprint('auth', __name__, template_folder='templates', url_prefix = "/a
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+@auth.route('/reset_password')
+def reset_password():
+    return render_template('auth/reset_password.html')
--- app/courses/routes.py
+++ app/courses/routes.py
@@ -2,4 +2,20 @@ from flask import render_template, request, Blueprint
 
 from flask import Blueprint
 
-courses = Blueprint('courses', __name__, template_folder='templates')
+courses = Blueprint('courses', __name__, template_folder='templates', url_prefix = '/courses')
+@courses.route('/add_course')
+def add_course():
+    return render_template('courses/add_course.html')
+
+@courses.route('/course_detail')
+def course_detail():
+    return render_template('courses/course_detail.html')
+
+@courses.route('/edit_course')
+def edit_course():
+    return render_template('courses/edit_course.html')
+
+@courses.route('/list_courses')
+def list_courses():
+    return render_template('courses/list_courses.html')
+
