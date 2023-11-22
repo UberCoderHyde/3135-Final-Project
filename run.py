a5672b41fa8fb98620b883a949d20f643855fc78
Shlok Singh <ssingh67@uncc.edu>
2023-11-22 14:53:24 -0500

    fixed module issues on requirements.txt and cleaned up base and home html
    also worked on a few changes in run.py

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
+
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        from app.extensions import db
-        db.create_all()
     app.run(debug=True)
