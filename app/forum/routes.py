beccc3f26324ef9c0e95e2e00e01df77ebdbfb1f
Colby0506 <Colby0506@hotmail.com>
2023-12-05 00:14:43 -0500

    fixed stupid forum stuff more to do tho

--- app/forum/routes.py
+++ app/forum/routes.py
@@ -12,11 +12,11 @@ def forum_home():
     return render_template('forum/forum_home.html', forums=forums)
 
 
-@forum.route('/forum/<int:forum_id>')
+@forum.route('/forum_home/<int:forum_id>')
 def forum_posts(forum_id):
     forum = Forum.query.get_or_404(forum_id)
     posts = ForumPost.query.filter_by(forum_id=forum.id).order_by(ForumPost.timestamp.desc()).all()
-    return render_template('forum/forum_posts.html', forum=forum, posts=posts)
+    return render_template('forum/forum_posts.html', forum=forum, posts=posts, forum_id=forum.id)
 
 @forum.route('/forum_topic/<int:post_id>', methods=['GET', 'POST'])
 def forum_topic(post_id):
@@ -71,3 +71,20 @@ def delete_post(post_id):
 
     flash('The post and all associated comments have been deleted.', 'success')
     return redirect(url_for('forum.forum_home'))
+@forum.route('/delete_forum/<int:forum_id>', methods=['POST'])
+def delete_forum(forum_id):
+    forum = Forum.query.get_or_404(forum_id)
+
+    # Check if the current user is the owner of the forum
+    if current_user == forum.user:
+        # Perform the deletion logic (You need to implement this part)
+        # For example, you might delete associated posts, topics, etc.
+        # Then delete the forum itself
+        db.session.delete(forum)
+        db.session.commit()
+
+        flash('Forum deleted successfully', 'success')
+    else:
+        flash('You do not have permission to delete this forum', 'danger')
+
+    return redirect(url_for('forum.forum_home')) 
