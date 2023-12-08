393114eb52e8384241700f263547aa28b0c2e468
Colby0506 <Colby0506@hotmail.com>
2023-12-08 01:21:36 -0500

    finished the edit o f user profiles

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,29 +1,8 @@
-from flask import render_template, redirect, url_for
-from app import app  #Flask app
-from .forms import EditProfileForm, EditUserProfileForm, ProfileForm
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from wtforms.validators import Length
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
-def edit_profile():
-    form = EditProfileForm()
-
-    if form.validate_on_submit():
-        # Process the form data, update the user profile, and redirect
-        return redirect(url_for('profile'))  # Redirect to the profile page
-
-    return render_template('edit_profile.html', form=form)
-
-@app.route('/edit_user_profile', methods=['GET', 'POST'])
-def edit_user_profile():
-    form = EditUserProfileForm()
-
-    if form.validate_on_submit():
-        # Process the form data, update the user profile, and redirect
-        return redirect(url_for('profile'))  # Redirect to the profile page
-
-    return render_template('edit_user_profile.html', form=form)
-
-@app.route('/profile')
-def profile():
-    # Fetch user data from your database or user authentication system
-    user = get_user_data()  # Replace with your method to get user data
-    return render_template('profile.html', user=user)
+class EditProfileForm(FlaskForm):
+    description = TextAreaField('Description', validators=[Length(max=1000)])
+    submit = SubmitField('Update Profile')
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,16 +1,26 @@
-from flask import render_template, request, Blueprint
-
+from .forms import EditProfileForm
+from flask import flash, redirect, render_template, request, Blueprint, url_for
+from flask_login import current_user, login_required
+from app.models import User
 from flask import Blueprint
+from app.extensions import db
 
 user = Blueprint('user', __name__, template_folder='templates',url_prefix="/user")
-@user.route('/change_password')
-def change_password():
-    return render_template('user/change_password.html')
 
-@user.route('/edit_password')
-def edit_password():
-    return render_template('user/edit_password.html')
-
-@user.route('/profile')
-def profile():
-    return render_template('user/profile.html')
+@user.route('/profile/<int:user_id>')
+def user_profile(user_id):
+    tutor = User.query.get_or_404(user_id)
+    return render_template('user/profile.html', user=user)
+@user.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.description = form.description.data
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('user.user_profile', user_id=current_user.id))
+    
+    elif request.method == 'GET':
+        form.description.data = current_user.description
+    return render_template('user/edit_profile.html', form=form)
